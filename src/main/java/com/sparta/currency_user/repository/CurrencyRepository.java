f81317e8afc29c19317ad4e8f426e17c53161443
heroKim997 <ghkufud6545@gmail.com>
2024-11-29 19:19:48 +0900

    add. postConstruct 추가, Spring 최초 실행 시, DB 값 검증 진행

--- src/main/java/com/sparta/currency_user/repository/CurrencyRepository.java
+++ src/main/java/com/sparta/currency_user/repository/CurrencyRepository.java
@@ -2,8 +2,14 @@ package com.sparta.currency_user.repository;
 
 import com.sparta.currency_user.entity.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CurrencyRepository extends JpaRepository<Currency, Long> {
+
+    @Query("select e from Currency e where e.exchangeRate <= 0")
+    List<Currency> findInvalidExchangeRates();
 }
--- src/main/java/com/sparta/currency_user/service/CurrencyService.java
+++ src/main/java/com/sparta/currency_user/service/CurrencyService.java
@@ -4,17 +4,25 @@ import com.sparta.currency_user.dto.CurrencyRequestDto;
 import com.sparta.currency_user.dto.CurrencyResponseDto;
 import com.sparta.currency_user.entity.Currency;
 import com.sparta.currency_user.repository.CurrencyRepository;
+import com.sparta.currency_user.repository.ExchangeRepository;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
+@Component
 public class CurrencyService {
 
     private final CurrencyRepository currencyRepository;
+    private final ExchangeRepository exchangeRepository;
 
     public CurrencyResponseDto findById(Long id) {
         return new CurrencyResponseDto(findCurrencyById(id));
@@ -30,7 +38,21 @@ public class CurrencyService {
 
     @Transactional
     public CurrencyResponseDto save(CurrencyRequestDto currencyRequestDto) {
+        if (currencyRequestDto.getExchangeRate().compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("환율은 0 보다 커야합니다." + currencyRequestDto.getExchangeRate());
+        }
         Currency savedCurrency = currencyRepository.save(currencyRequestDto.toEntity());
         return new CurrencyResponseDto(savedCurrency);
+    }
+
+    //PostConstruct
+    @PostConstruct
+    public void validExchangeRate() {
+        List<Currency> invalidRates = currencyRepository.findInvalidExchangeRates();
+        if(!invalidRates.isEmpty()) {
+            for(Currency currency : invalidRates) {
+                log.info("잘못된 환율이 입력되어 있습니다. : id={}, rate={}", currency.getId(), currency.getExchangeRate());
+            }
+        }
     }
 }
